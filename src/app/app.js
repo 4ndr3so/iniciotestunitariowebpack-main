b68e8fdd4783aaacf8920b1b4202c8f0dca29002
andres <lp.carlosorjuela@gmail.com>
2024-12-17 15:10:35 -0500

    shorter distance

--- src/app/app.js
+++ src/app/app.js
@@ -15,6 +15,25 @@ function TreeNode(val, left, right) {
 }
 
 export const prueba = {
+    shortestDistance(wordsDict, word1, word2) {
+        let index1 = -1;
+        let index2 = -1;
+        let minDistance = Infinity;
+    
+        for (let i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i] === word1) {
+                index1 = i;
+            }
+            if (wordsDict[i] === word2) {
+                index2 = i;
+            }
+            if (index1 !== -1 && index2 !== -1) {
+                minDistance = Math.min(minDistance, Math.abs(index1 - index2));
+            }
+        }
+    
+        return minDistance;
+    },
     isAnagram2(s, t) {
         if (s.length !== t.length) return false;
         const map = new Map();
