5808c2b1e4d8b588b35f48c53f48c1850cf045b0
andres <lp.carlosorjuela@gmail.com>
2025-01-14 10:24:42 -0500

    first unique letter2

--- src/app/app.js
+++ src/app/app.js
@@ -19,6 +19,18 @@ function isBadVersion(version) {
 }
 
 export const prueba = {
+    firstUniqChar2(s) {
+        const map = new Map();
+        for (let i = 0; i < s.length; i++) {
+            map.set(s[i], (map.get(s[i]) || 0) + 1);
+        }
+        for (let i = 0; i < s.length; i++) {
+            if (map.get(s[i]) === 1) {
+                return i;
+            }
+        }
+        return -1;
+    },
     firstUniqChar(s) {
         const map = new Set();
         for (let i = 0; i < s.length; i++) {
