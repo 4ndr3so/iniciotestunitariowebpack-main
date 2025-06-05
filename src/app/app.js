2edb8ce4a359bc10e3a53b8a9e2ae60f42700caa
andres <lp.carlosorjuela@gmail.com>
2025-06-05 14:36:57 -0400

    jumps

--- src/app/app.js
+++ src/app/app.js
@@ -50,6 +50,26 @@ function isBadVersion(version) {
 }
 
 export const prueba = {
+    jump(nums) {
+        let jumps = 0;
+        let currentEnd = 0;
+        let farthest = 0;
+
+        for (let i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i === currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+
+                if (currentEnd >= nums.length - 1) {
+                    break;
+                }
+            }
+        }
+
+        return jumps;
+    },
     generateParenthesis(n) {
         const result = [];
         function backTrack(current, open, close) {
